a460eafd1aa0833bb1fba58c7f991ca519b7f31f
Andre Scutieri <ascutieri@proton.me>
2026-06-18 22:56:45 -0300

    feat: generateSlugFromId util helper

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,3 +11,9 @@ export function generateTextFromDate(text: string): string {
     const date = new Date(newText);
 	return `${date.getFullYear()} - ${capitalize(date.toLocaleDateString('pt-BR',{month: 'long'}))}`;
 }
+
+export function generateSlugFromId(text: string): string {
+	const regex = /^(\d{4})-(\d{2})-(\d{2})-(.+)/;
+	const pathParts = regex.exec(text);
+	return pathParts?.slice(1,5).join("/") || "";
+}
